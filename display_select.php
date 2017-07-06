63809e8ef211938744f7c06ebade35789b6601e5
aruskey <aruskey@shaw.ca>
2017-07-06 16:05:29 -0700

    first working version of display_select

--- display_select.php
+++ display_select.php
@@ -1,6 +1,6 @@
-<?php include functions.php; ?>
+<?php include "functions.php"; ?>
 
-?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -13,13 +13,32 @@
     background-size:cover;
     height:100vh;
     }
+    .maincontainer{
+        position:absolute;
+        top:0;
+        bottom:0;
+        left:0;
+        right:0;
+        margin:auto;
+        height:200px;
+        width: 500px;
+    }
 </style>
-    <meta http-equiv="refresh" content = "10" /> 
     <title>Document</title>
 </head>
 <body>
 
-   nothing to see yet
+<div class="container-fluid maincontainer">
+ <div class="card card-block">
+    <form action="display_select.php" method="post">
+       <h1>Select Display Option </h1>
+        <input type="checkbox" name="flower"> Flower <br>
+        <input type="checkbox" name="concentrates"> Concentrates <br> 
+        <input type="submit" name-"submit" value="Submit" class="btn btn-primary">
+    </form>
+ </div>        
+</div>
     
+            
 </body>
 </html>
