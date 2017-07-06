63c7edb9bf0b25a3c3a41684c2819ee13ccbb4d0
aruskey <aruskey@shaw.ca>
2017-07-06 15:30:20 -0700

    Initial Commit

---
+++ sign_display.php
@@ -0,0 +1,22 @@
+<?php include functions.php; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<style>
+   body{
+    background-image:url("https://thegreenhart.com/wp-content/uploads/2016/06/about-image-9.jpg");
+    background-repeat: no-repeat;
+    background-size:cover;
+    height:100vh;
+    }
+</style>
+    <title>Document</title>
+</head>
+<body>
+
+   nothing to see yet
+    
+</body>
+</html>
